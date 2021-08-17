93491d6b7b8bd64901f6ac109a373759dc395b09
Alfredo de Morais <alfredneto@hotmail.com>
2021-08-17 20:45:50 -0300

    Included on viewsets.py the function permissions, It has the main purpose to request authenticate for use the API

--- api/erp/viewsets.py
+++ api/erp/viewsets.py
@@ -2,14 +2,17 @@ from rest_framework import viewsets
 from .serializers import PurchaseDetailSerializer
 from .serializers import ProductsSerializer
 from .models import PurchaseDetail, Products
+from rest_framework import permissions
 
 
 # define viewsets for classes created on serializers.py
 class PurchaseDetailViewSet(viewsets.ModelViewSet):
     serializer_class = PurchaseDetailSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = PurchaseDetail.objects.all()
 
 
 class ProductsViewSet(viewsets.ModelViewSet):
     serializer_class = ProductsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Products.objects.all()
