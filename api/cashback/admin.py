59e1758c9e87824610708f8db4da4b5cebec3a04
Alfredo de Morais <alfredneto@hotmail.com>
2021-08-18 20:48:23 -0300

    include the models Cashback_API on admin.py file

--- api/cashback/admin.py
+++ api/cashback/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Customers, Cashback_API
+
 
 # Register your models here.
+class Cashback_APIAdmin(admin.ModelAdmin):
+    """
+    That class provide for admin interface a good reference
+    from database Cashback_API.
+    """
+    list_display = ('customer_name', 'customer_document',
+                    'message', 'cashback_amount')
