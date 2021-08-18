210a8e8d1811c61158e867384df77e11aea63121
Alfredo de Morais <alfredneto@hotmail.com>
2021-08-18 20:27:51 -0300

    Difined  models.py from API MaisTodos

--- api/cashback/models.py
+++ api/cashback/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Cashback_API(models.Model):
+    """
+    This is model to renderize the Cashback_API
+    """
+    customer_name = models.CharField(max_length=50)
+    customer_document = models.CharField(max_length=11)
+    message = models.CharField(max_length=255)
+    cashback_amount = models.FloatField(default=0)
+
+
+class Customers(models.Model):
+    """
+    This is model to check customers document (CPF)
+    """
+    customer_document = models.CharField(max_length=11)
