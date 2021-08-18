caea2ab6239292c24cd12d56be9efc23f8a623ee
Alfredo de Morais <alfredneto@hotmail.com>
2021-08-17 22:11:00 -0300

    It was rewrite some code for post method. This functions post has the main purpose check customers document

--- api/erp/viewsets.py
+++ api/erp/viewsets.py
@@ -3,6 +3,11 @@ from .serializers import PurchaseDetailSerializer
 from .serializers import ProductsSerializer
 from .models import PurchaseDetail, Products
 from rest_framework import permissions
+from .functions import (check_cpf_digits,
+                        check_cpf_isvalid,
+                        cachback_calculate,
+                        calculate_check
+                        )
 
 
 # define viewsets for classes created on serializers.py
@@ -10,6 +15,22 @@ class PurchaseDetailViewSet(viewsets.ModelViewSet):
     serializer_class = PurchaseDetailSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = PurchaseDetail.objects.all()
+
+    def post(self, request, format=None):
+        """
+        This function will provide double check for customers documents
+        and return true in case of sucessuful validated. 
+        """
+        api_data = request.data
+        customers_document = api_data["results"]["customer_document"]
+        first_validate = check_cpf_digits(customers_document)
+        second_validate = check_cpf_isvalid(customers_document)
+        if (first_validate and second_validate) == True:
+            message = "Customers Document was sucessuful validated"
+            return message
+        else:
+            message = "Customers Document error validated"
+            return message
 
 
 class ProductsViewSet(viewsets.ModelViewSet):
