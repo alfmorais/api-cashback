e214f4ec0791b0efb90bfa2fe8963917297f464e
Alfredo de Morais <alfredneto@hotmail.com>
2021-08-22 22:04:44 -0300

    Started to write a code regarding to CREATE method from second API

--- api/cashback/viewsets.py
+++ api/cashback/viewsets.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import CustomersSerializer
 from .models import Customers
+from rest_framework.response import Response
 
 
 # define viewsets for classes created on serializers.py
@@ -10,5 +11,18 @@ class CustomersViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Customers.objects.all()
 
-    def post(self, request, format=None):
-        pass
+    def create(self, request, format=None):
+        """
+        This method CREATE function will responsable for
+        show all cashback value by customers
+        """
+
+        # Request data from POST API
+        data = request.data
+
+        # request value from POST method
+        customer_document = data["customer_document"]
+
+        # getting database regarding cashback
+
+        return Response("OK")
